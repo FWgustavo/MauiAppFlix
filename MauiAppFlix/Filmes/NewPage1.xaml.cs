5bb1a290ae8de4e5e66799bc61e67e498d19e30e
Usuario <Usuario@CPD01-M06>
2024-05-02 15:39:06 -0300

    inicio

---
+++ MauiAppFlix/Filmes/NewPage1.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppFlix.Filmes;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiAppFlix/Posters/NewPage1.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppFlix.Posters;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
